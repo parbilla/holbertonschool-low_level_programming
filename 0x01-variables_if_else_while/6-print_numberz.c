4574c9bc816015073f575d8d6ff93892c0bdcef5
parbilla <parbilla@gmail.com>
2020-09-17 18:10:20 +0000

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* Get the alphabet */
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
