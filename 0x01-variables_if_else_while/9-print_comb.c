a3ba5d2edf38ff37391aa3d8a77e4e7c53c8a544
parbilla <parbilla@gmail.com>
2020-09-17 19:38:31 +0000

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* Get the numbers */
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
