c0ba0d69810e643332473a5f3224969a88e7d9e2
parbilla <parbilla@gmail.com>
2020-09-17 18:59:20 +0000

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* Get the alphabet */
+	int c;
+
+	for (c = 122; c >= 97; c--)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
