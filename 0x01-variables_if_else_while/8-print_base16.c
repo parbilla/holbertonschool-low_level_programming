ab05f22f3050563069dc387f0a3623e138b6f776
parbilla <parbilla@gmail.com>
2020-09-17 19:23:15 +0000

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* Get the hexadecimal */
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+	}
+	for (c = 97; c <= 102; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
