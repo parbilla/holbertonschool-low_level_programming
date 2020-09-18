568ef78590c309b6cf45c60a22ca3b1b4ca2f4dc
parbilla <parbilla@gmail.com>
2020-09-18 03:04:04 +0000

    task 13

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* Get the numbers */
+	int c, d, e, f, g, h;
+
+	for (c = 48; c <= 57; c++)
+	{
+		for (d = 48; d <= 57; d++)
+		{
+			for (e = 48; e <= 57; e++)
+			{
+				for (f = 48; f <= 57; f++)
+				{
+					g = c * 10 + d;
+					h = e * 10 + f;
+					if (g < h)
+					{
+					putchar(c);
+					putchar(d);
+					putchar(' ');
+					putchar(e);
+					putchar(f);
+					if (g == 98 && h == 99)
+					{
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
