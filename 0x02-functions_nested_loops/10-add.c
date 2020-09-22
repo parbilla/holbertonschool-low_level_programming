adf4f66d1407fa8f27ca5673bb4d5dbecde2858b
parbilla <parbilla@gmail.com>
2020-09-22 18:55:36 +0000

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - print sum
+ * @a: sum parameter
+ * @b: sum parameter
+ *
+ * Return: result (Success)
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
