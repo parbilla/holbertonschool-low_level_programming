74975b4f6a4fdc481909aa504094dadb4c59f8d0
parbilla <parbilla@gmail.com>
2020-09-22 19:19:10 +0000

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print to 98
+ * @n: parameter
+ *
+ * Return: Always (Success)
+ */
+
+void print_to_98(int n)
+{
+	int count;
+
+	if (n <= 98)
+	{
+		for (count = n; count < 99; count++)
+		{
+			printf("%i", count);
+			if (count != 98)
+			{
+				printf(",");
+			}
+		}
+	}
+	else
+	{
+		for (count = n; count > 97; count--)
+		{
+			printf("%i", count);
+			if (count != 98)
+			{
+				printf(",");
+			}
+		}
+	}
+	printf("\n");
+}
