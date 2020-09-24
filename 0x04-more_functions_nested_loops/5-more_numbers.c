775a8e3d40ad9d9c26694c978a891959111b3cad
parbilla <parbilla@gmail.com>
2020-09-24 15:09:47 +0000

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+void more_numbers(void)
+{
+	char t, c, d;
+
+	for (t = '0'; t <= '9'; t++)
+	{
+		for (c = '0'; c <= '1'; c++)
+		{
+			for (d = '0'; d <= '9'; d++)
+			{
+				if (c != '0' && d < '5')
+				{
+					_putchar(c);
+				}
+			        if (c == '0' || (c == '1' && d <= '4'))
+				{
+					_putchar(d);
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
