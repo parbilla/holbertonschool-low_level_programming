bdda8d27ac9c336bf70947af592a55bb0f2a3aae
parbilla <parbilla@gmail.com>
2020-09-24 13:16:01 +0000

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * mul - print multiplication
+ * @a: mult parameter
+ * @b: mult parameter
+ *
+ * Return: result (Success)
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
