cb16ccc4ad3f06b67eb4e58e82e772182ce10f65
parbilla <parbilla@gmail.com>
2020-09-24 13:07:58 +0000

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _isupper - define uppercase
+ * @c: parameter
+ *
+ * Return: 1 (Success)
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
