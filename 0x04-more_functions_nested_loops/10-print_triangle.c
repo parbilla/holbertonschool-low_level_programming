f9291576c6e9e0b1f5ad5d23dca47c99aaa28db2
parbilla <parbilla@gmail.com>
2020-09-24 20:24:41 +0000

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - print triangle
+ * @size: number of hashtags
+ *
+ * Return: result (Success)
+ */
+void print_triangle(int size)
+{
+	int a, b;
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 1; b <= size; b++)
+		{
+			if (b == size)
+			{
+				_putchar('#');
+				_putchar('\n');
+		}
+		else
+		{
+			if (a < size - b)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('#');
+			}
+		}
+	}
+}
