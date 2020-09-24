b0c4d23ea36872e3f460743b68bb4bb54924ce5e
parbilla <parbilla@gmail.com>
2020-09-24 13:42:39 +0000

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
