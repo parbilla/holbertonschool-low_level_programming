5da1d68a32ab09511dc98c9140d87f07e2e36610
parbilla <parbilla@gmail.com>
2020-09-29 16:13:23 +0000

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * rev_string - print string in reverse
+ * @s: parameter
+ *
+ * Return: always (Success)
+ */
+
+void rev_string(char *s)
+{
+	int final, principio;
+
+	char aux;
+
+	final = 0;
+	while (s[final] != '\0')
+	{
+		final++;
+	}
+       	principio = 0;
+	while (principio < final)
+	{
+		aux = s[final - 1];
+		s[final - 1] = s[principio];
+		s[principio] = aux;
+		final--;
+		principio++;
+	}
+}
