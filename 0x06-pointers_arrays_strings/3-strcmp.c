8ef2663a6b9f4e248aff236334c854bca8ad9f14
parbilla <parbilla@gmail.com>
2020-10-01 16:30:16 +0000

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,7 +10,7 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int len1, len2;
+	int len1, len2, i, dif;
 
 	for (len1 = 0; s1[len1] != 0; len1++)
 	{
@@ -18,16 +18,13 @@ int _strcmp(char *s1, char *s2)
 	for (len2 = 0; s2[len2] != 0; len2++)
 	{
 	}
-	if (len1 < len2)
+	for (i = 0; i < len1 && i < len2; i++)
 	{
-		return (-15);
+		if (s1[i] != s2[i])
+		{
+			dif = s1[i] - s2[i];
+			return (dif);
+		}
 	}
-	else if (len1 > len2)
-	{
-		return (15);
-	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
