e5f98c300e712251377797610c62a834000d08f6
parbilla <parbilla@gmail.com>
2020-10-05 19:43:55 +0000

    5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * _strstr - searches a string
+ * @haystack: string
+ * @needle: substring
+ *
+ * Return: substring
+ */
+
+char  *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != 0; i++)
+	{
+		if (needle[0] == haystack[i])
+		{
+			for (j = 1; needle[j] != 0; j++)
+			{
+				if (needle[j] != haystack[i + j])
+			{
+				break;
+			}
+			}
+		}
+		if (needle[j + 1] == 0)
+		{
+			return (haystack + i);
+		}
+	}
+	return (0);
+}
