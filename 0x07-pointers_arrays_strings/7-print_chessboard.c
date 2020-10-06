1fab3a810117651c2f4f81f01d82157aade59fe3
parbilla <parbilla@gmail.com>
2020-10-06 02:15:14 +0000

    7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -13,10 +13,13 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; i < 8; i++)
 	{
-		for (j = 1; j < 8; j++)
+		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
+			if (j == 7)
+			{
+				_putchar(0);
+			}
 		}
-		_putchar(0);
 	}
 }
