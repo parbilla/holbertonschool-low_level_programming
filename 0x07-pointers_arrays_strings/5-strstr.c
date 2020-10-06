57df743b8c7e59bce687c01c451554b6c98869e2
parbilla <parbilla@gmail.com>
2020-10-06 02:04:37 +0000

    5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,21 +12,22 @@ char  *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
+	if (*needle == 0)
+	{
+		return (haystack);
+	}
 	for (i = 0; haystack[i] != 0; i++)
 	{
 		if (needle[0] == haystack[i])
 		{
-			for (j = 1; needle[j] != 0 && haystack[i + j] != 0; j++)
+			for (j = 1; needle[j] != 0; j++)
 			{
 				if (needle[j] != haystack[i + j])
 			{
 				break;
 			}
+			return (&haystack[i]);
 			}
-		}
-		if (needle[j + 1] == 0)
-		{
-			return (haystack + i);
 		}
 	}
 	return (0);
