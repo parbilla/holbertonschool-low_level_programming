b5660b00293394a9cad619fb94f8ec33771ae042
parbilla <parbilla@gmail.com>
2020-10-06 01:07:53 +0000

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,13 +9,13 @@
  * Return: pointer to dest
  */
 
-char *_memcpy(char *dest, char b, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		s[i] = b;
+		dest[i] = src[i];
 	}
-	return (s);
+	return (dest);
 }
