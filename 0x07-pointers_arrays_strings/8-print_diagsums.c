811d4d56dd0b7a18ae103a72350a8424b54e8d5d
parbilla <parbilla@gmail.com>
2020-10-06 15:42:15 +0000

    8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -16,7 +16,7 @@ void print_diagsums(int *a, int size)
 	for (i = 0; (i * size + i) < (size * size); i++)
 	{
 		sum1 = sum1 + a[(i * size + i)];
-		sum2 = sum2 + a[((size * i) + size - 1 + i)];
+		sum2 = sum2 + a[((size * i) + size - 1 - i)];
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
