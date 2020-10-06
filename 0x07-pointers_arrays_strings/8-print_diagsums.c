774241c551d70f4e445514aa988853e9b3c010b6
parbilla <parbilla@gmail.com>
2020-10-06 15:38:06 +0000

    8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints diagonal sums
+ * @a: pointer
+ * @size: size
+ *
+ * Return: always
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, sum1, sum2;
+
+	for (i = 0; (i * size + i) < (size * size); i++)
+	{
+		sum1 = sum1 + a[(i * size + i)];
+		sum2 = sum2 + a[((size * i) + size - 1 + i)];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
