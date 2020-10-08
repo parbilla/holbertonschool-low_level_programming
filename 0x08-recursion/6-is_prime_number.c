aa3fd087f935042bea94aa3f2abc94ef599e4b90
parbilla <parbilla@gmail.com>
2020-10-08 02:31:35 +0000

    6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - looks for a prime number
+ * @n: number
+ *
+ * Return: result
+ */
+
+int is_prime_number(int n)
+{
+	if (n == 1)
+	{
+		res = 1;
+	}
+	else if (n > 3)
+	{
+		if (n / div == div)
+		{
+			res = div;
+		}
+		else
+		{
+			div--;
+			_sqrt_recursion
+		}
+	}
+	else
+	{
+		res = -1;
+	}
+	return (res);
+}
