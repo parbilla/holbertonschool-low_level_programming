d1f1e48fa339e9dc6e349426810314098988ec06
parbilla <parbilla@gmail.com>
2020-10-11 15:17:23 +0000

    4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - add numbers
+ * @argc: count
+ * @argv: numbers
+ *
+ * Return: result
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, res;
+
+	res = 0;
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(*argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			res += atoi(argv[i]);
+		}
+	}
+	printf("%i\n", res);
+	return (0);
+}
