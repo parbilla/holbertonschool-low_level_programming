d2f44aad725bb90aaf79ff40e1a42c0c51976a20
parbilla <parbilla@gmail.com>
2020-10-11 12:52:54 +0000

    1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: count
+ * @argv: string
+ *
+ * Return: result
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
