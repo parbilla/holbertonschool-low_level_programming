175d33a1245d6ecc6bee155a6e9039e75ab09cc8
parbilla <parbilla@gmail.com>
2020-10-15 15:41:53 +0000

    0x0C

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - returns pointer
+ * @b: length
+ *
+ * Return: pointed to dest
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void p*;
+
+	p = malloc(b);
+	if (p == 0)
+	{
+		exit (98);
+	}
+	return (p);
+}
