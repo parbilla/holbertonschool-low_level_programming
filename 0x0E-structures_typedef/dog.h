948924ad7659dcd6461ac202f0ce5f1a4713f78b
parbilla <parbilla@gmail.com>
2020-10-19 18:59:08 +0000

    structs

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef TEST
+#define TEST
+#define dog
+
+/**
+ * struct dog - dog description
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * dogs description:
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+my_dog;
+
+#endif
