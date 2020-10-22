1641b4ba114348d0485c27520b1e2ba678f54b83
parbilla <parbilla@gmail.com>
2020-10-22 19:45:46 +0000

    advanced

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program
+ * @argc: row
+ * @argv: col
+ *
+ * Return: pointer
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+	unsigned char *p = (char *) main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	i = 0;
+	while (i < atoi(argv[1]))
+	{
+		printf("%02x ", p[i]);
+		i++;
+	}
+	printf("\b\n");
+}
