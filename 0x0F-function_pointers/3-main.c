ba5dd07dbefd158df56ca981c166159c1b39ed12
parbilla <parbilla@gmail.com>
2020-10-22 15:11:43 +0000

    recovering

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,22 +18,12 @@ int main(int argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("Error\n");
-		return (98);
+		exit(98);
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	operator = argv[2];
-	if (*operator != '+' || *operator != '-' || *operator != '*' ||
-	    *operator != '/' || *operator != '%')
-	{
-		printf("Error\n");
-		return (99);
-	}
-	if ((*operator == '/' || *operator == '%') && (argv[3] == 0))
-	{
-		printf("Error\n");
-		return (100);
-	}
+
 	printf("%d", (*get_op_func(operator))(num1, num2));
 	return (0);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,12 +1,8 @@
 #include "3-calc.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
  * op_add - addition
- * op_sub - substraction
- * op_mul - multiplication
- * op_div - division
- * op_mod - modular
  * @a: row
  * @b: col
  *
@@ -18,22 +14,64 @@ int op_add(int a, int b)
 	return (a + b);
 }
 
+/**
+ * op_sub - substraction
+ * @a: row
+ * @b: col
+ *
+ * Return: pointer
+ */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
+/**
+ * op_mul - multiplication
+ * @a: row
+ * @b: col
+ *
+ * Return: pointer
+ */
 
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+ * op_div - division
+ * @a: row
+ * @b: col
+ *
+ * Return: pointer
+ */
+
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 
+/**
+ * op_mod - modular
+ * @a: row
+ * @b: col
+ *
+ * Return: pointer
+ */
+
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
 	return (a % b);
 }
