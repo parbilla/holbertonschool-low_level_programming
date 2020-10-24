268093e017afacf3e174718d8197d49676ebcb8b
parbilla <parbilla@gmail.com>
2020-10-24 21:27:09 +0000

    10

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,104 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 
+/**
+ * struct op - struct print
+ *
+ * @c: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *c;
+	void (*f)();
+} prt;
+
+/**
+ * get_prt - select type
+ * @s: char
+ *
+ * Return: pointer
+ */
+
+void (*get_prt(char *s))()
+{
+	prt opc[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_fl},
+		{"s", print_str},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (opc[i].c != NULL)
+	{
+		if (strcmp(opc[i].c, s) == 0)
+		{
+			return (opc[i].f);
+		}
+		i++;
+	}
+}
+
+/**
+ * print_char - print char
+ * @c: char
+ *
+ * Return: pointer
+ */
+
+void print_char(char c)
+{
+	printf(a + b);
+}
+
+/**
+ * op_sub - substraction
+ * @a: row
+ * @b: col
+ *
+ * Return: pointer
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication
+ * @a: row
+ * @b: col
+ *
+ * Return: pointer
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: row
+ * @b: col
+ *
+ * Return: pointer
+ */
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
 /**
  * print_all - program
  * @format: arguments
