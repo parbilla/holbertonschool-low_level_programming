6f6fe73b04f53444583a140967dbb841b392a6fc
parbilla <parbilla@gmail.com>
2020-10-26 01:59:12 +0000

    fixing

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,8 @@
 #ifndef VARIADIC
 #define VARIADIC
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
