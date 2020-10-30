e2042546231d249e9e01a5def8584a010b7e5739
parbilla <parbilla@gmail.com>
2020-10-30 19:02:52 +0000

    task 3

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_list - inserts node at the end.
+ * @head: pointer to pointer
+ *
+ * Return: adress of new element.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *aux;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	while (head)
+	{
+		aux = head->next;
+		free(head);
+		head = aux;
+	}
+}
