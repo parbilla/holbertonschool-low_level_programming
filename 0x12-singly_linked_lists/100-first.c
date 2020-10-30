86f931e3f50e62d296958e2266230264b46cc86e
parbilla <parbilla@gmail.com>
2020-10-30 19:44:26 +0000

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * print_before - prints before the main function is executed
+ *
+ * Return: Nothing
+ */
+
+void print_before(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
