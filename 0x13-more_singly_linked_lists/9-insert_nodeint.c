fe63cdb7fe2e4a0794850d251f4b8059f530643d
parbilla <parbilla@gmail.com>
2020-11-04 17:17:57 +0000

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,8 +13,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
 	listint_t *node, *aux;
-	aux = *head;
 
+	aux = *head;
 	if (!*head)
 	{
 		return (NULL);
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		node->next = aux->next;
-		aux = node;
+		*head = node;
 	}
 	else
 	{
