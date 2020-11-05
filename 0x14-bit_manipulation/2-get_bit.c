93d8178e91d9ad5eda206c8c87e114f6104fd132
parbilla <parbilla@gmail.com>
2020-11-05 23:39:10 +0000

    2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * get_bit - the value of a bit at a given index.
+ * @index:index
+ * @n: number
+ * Return: void.
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	n = n >> index;
+	if (n == 0)
+	{
+		return (-1);
+	}
+	if (n & 1)
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}
