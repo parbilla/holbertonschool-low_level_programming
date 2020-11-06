f44306c586e98b2f35436a626d60968f2d00bb6a
parbilla <parbilla@gmail.com>
2020-11-06 14:01:09 +0000

    2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,11 +9,11 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	n = n >> index;
-	if (n == 0)
+	if (index > sizeof(unsigned int) * 8));
 	{
-		return (-1);
-	}
+                return (-1);
+        }
+	n = n >> index;
 	if (n & 1)
 	{
 		return (1);
