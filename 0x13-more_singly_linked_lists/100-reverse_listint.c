a58e834bce5972756b8216eba854d26a9d9e16d2
parbilla <parbilla@gmail.com>
2020-11-07 19:14:25 +0000

    adv

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * reverse _listint - inserts a new node.
+ * reverse_listint - reverses a list.
  * @head: pointer to list head
  *
  * Return: pointer to list node.
@@ -9,48 +9,23 @@
 
 listint_t *reverse_listint(listint_t **head)
 {
-	unsigned int i;
 	listint_t *aux1, *aux2;
 
-	if (*head && head->next)
-	{
-	aux1 = head;
-	aux2 = head->next;
-	aux1->next = NULL;
-
-	head = aux2;
-	aux2 = aux2->next;
-	head->next= aux1;
-	aux1 = head;
-
-
-	node = malloc(sizeof(listint_t));
-	if (!node)
+	if (*head == NULL)
 	{
 		return (NULL);
 	}
-	node->n = n;
-	if (!*head || idx == 0)
+	aux1 = *head;
+	aux2 = (*head)->next;
+	aux1->next = NULL;
+	while (aux2->next != NULL)
 	{
-		node->next = *head;
-		*head = node;
+		*head = aux2;
+		aux2 = (*head)->next;
+		(*head)->next = aux1;
+		aux1 = *head;
 	}
-	else
-	{
-		for (i = 0; i < idx - 1; i++)
-		{
-			if (!aux->next)
-			{
-				return (NULL);
-			}
-			aux = aux->next;
-		}
-		if (!aux)
-		{
-			return (NULL);
-		}
-		node->next = aux->next;
-		aux->next = node;
-	}
-	return (node);
+	*head = aux2;
+	(*head)->next = aux1;
+	return (*head);
 }
