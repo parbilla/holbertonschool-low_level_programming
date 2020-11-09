ffdf41c9a21118c93fb7afdbefc2258674a4679f
parbilla <parbilla@gmail.com>
2020-11-09 22:12:54 +0000

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,8 +26,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	write(fd, text_content, i);
-
+	if (text_content != NULL)
+	{
+		write(fd, text_content, i);
+	}
 	close(fd);
 	return (1);
 }
