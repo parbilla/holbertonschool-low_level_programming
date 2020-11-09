2163150e3e4b604d4986688130364a529c054385
parbilla <parbilla@gmail.com>
2020-11-09 22:33:06 +0000

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (fd == -1)
 	{
 		return (-1);
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 		{
 			i++;
 		}
-		write(fd, text_content, i + 1);
+		write(fd, text_content, i);
 	}
 	close(fd);
 	return (1);
