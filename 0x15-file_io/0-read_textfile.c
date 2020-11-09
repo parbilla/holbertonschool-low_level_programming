91901907995db568d65e5a047b5b63371543fac0
parbilla <parbilla@gmail.com>
2020-11-09 20:09:25 +0000

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,30 +4,37 @@
  * read_textfile - reads a text file and prints it to the standard output.
  * @filename: string to print
  * @letters: number of characters
- * Return: letters.
+ * Return: number of char printed.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	unsigned int i;
-	char buf[1024];
+	char *buf;
 
-	i = 0;
-	fd = open("file.txt", O_CREAT | O_RDONLY, 0500);
-	if (fd == -1 || filename == NULL)
+	if (filename == NULL)
 	{
 		return (0);
 	}
-	for (i = 0; i < letters; i++)
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
 	{
-		if (filename[i] == EOF)
-		{
-			return (0);
-		}
-		write(STDOUT_FILENO, filename[i], letters);
+		return (0);
 	}
-	read(fd, buf, letters);
-	close (fd);
-	return (letters);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	i = read(fd, buf, letters);
+	buf[i + 1] = '\0';
+
+	write(STDOUT_FILENO, buf, i);
+
+	close(fd);
+	free(buf);
+	return (i);
 }
