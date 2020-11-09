cc14463fcdf6fe1712543d616c33af78173a45e6
parbilla <parbilla@gmail.com>
2020-11-09 22:09:26 +0000

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * create_file - creates a file.
+ * @filename: file to create
+ * @text_content: text to put in file
+ * Return: 1 on success.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int i;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; text_content[i] != 0; i++)
+	{
+	}
+
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	write(fd, text_content, i);
+
+	close(fd);
+	return (1);
+}
