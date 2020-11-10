7efcffab80d6d2004f72fc296b46a4330db3130c
parbilla <parbilla@gmail.com>
2020-11-10 18:58:27 +0000

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -32,7 +32,7 @@ int main(int argc, char **argv)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-		else if (leer > 1)
+		else if (leer >= 1)
 		{
 			escr = write(fdto, buffer, leer);
 			if (escr == -1)
