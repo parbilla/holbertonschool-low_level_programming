2a17f247886dca6a16216b9da518b4041643cfd0
parbilla <parbilla@gmail.com>
2020-11-10 19:09:11 +0000

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -26,7 +26,7 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]), exit(99);
 	while (leer > 0)
 	{
-		read(fdfrom, buffer, 1024);
+		leer = read(fdfrom, buffer, 1024);
 		if (leer == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
