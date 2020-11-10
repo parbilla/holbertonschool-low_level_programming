b6fa7d5cc9376afff007b0e3752d93f6b787fc37
parbilla <parbilla@gmail.com>
2020-11-10 19:33:52 +0000

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,11 +3,11 @@
 /**
  * main - copies file to file.
  * @argc: number of file
- * @argv: files to copy
+ * @av: files to copy
  * Return: 1 on success.
  */
 
-int main(int argc, char **argv)
+int main(int argc, char **av)
 {
 	int fdfrom, fdto, escr, clto, clfr;
 	char *buffer[1024];
@@ -15,21 +15,20 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	fdfrom = open(argv[1], O_RDONLY);
-	fdto = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	fdfrom = open(av[1], O_RDONLY);
+	fdto = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fdfrom == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	}
 	if (fdto == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]), exit(99);
 	while (leer > 0)
 	{
 		leer = read(fdfrom, buffer, 1024);
 		if (leer == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 		}
 		else if (leer >= 1)
@@ -37,7 +36,7 @@ int main(int argc, char **argv)
 			escr = write(fdto, buffer, leer);
 			if (escr == -1)
 			{
-				dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+				dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]);
 				exit(99);
 			}
 		}
@@ -47,6 +46,6 @@ int main(int argc, char **argv)
 	if (clto == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", clto), exit(100);
 	if (clfr == -1)
-                dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", clfr), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", clfr), exit(100);
 	return (0);
 }
