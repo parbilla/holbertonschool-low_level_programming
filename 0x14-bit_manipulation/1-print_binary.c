ea271163b4e60376506bc9e0e1f3ecb5de83d483
parbilla <parbilla@gmail.com>
2020-11-30 18:25:44 +0000

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,18 +1,20 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  * print_binary - function that prints the binary representation of a number.
- * @n: number
+ * @n: number.
  * Return: void.
  */
 
 void print_binary(unsigned long int n)
 {
+	/* moves as long there is a 1 */
 	if (n > 1)
 	{
+		/* loses last digit and repeats operation until first one remains */
 		print_binary(n >> 1);
 	}
+	/* compares first digit and prints 1 or 0, and goes until last one */
 	if (n & 1)
 	{
 		_putchar('1');
