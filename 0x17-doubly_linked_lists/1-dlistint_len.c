43b9b502f598ab794557014cd2b5a93260202d97
parbilla <parbilla@gmail.com>
2020-12-14 11:47:26 +0000

    task 2

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - function that returns the number of elements in a linked dlistint_t list.
+ * @h: pointer to dlistint_t
+ *
+ * Return: number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	unsigned int cont;
+
+	for (cont = 0; h != NULL; cont++)
+	{
+		/* print each int of the dlistint */
+		h = h->next;
+	}
+	return (cont);
+}
