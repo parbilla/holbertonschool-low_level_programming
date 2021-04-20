1bd68bd86a0f281c7c2d6af6bc7e47111b6abf22
parbilla <parbilla@gmail.com>
2021-04-20 16:10:52 +0000

    .h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,5 +5,6 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int find_index(int *array, int value, int low, int high, size_t size);
 
 #endif /* SEARCH_H */
