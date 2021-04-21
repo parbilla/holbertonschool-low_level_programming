ca38cfcb4080162c916c1134c75a70cac73dd65c
parbilla <parbilla@gmail.com>
2021-04-21 13:54:52 +0000

    tasks

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,6 +5,7 @@
 #include <math.h>
 
 int linear_search(int *array, size_t size, int value);
+void print_check(size_t index, int integ);
 int binary_search(int *array, size_t size, int value);
 int find_index(int *array, int value, int low, int high, size_t size);
 int jump_search(int *array, size_t size, int value);
