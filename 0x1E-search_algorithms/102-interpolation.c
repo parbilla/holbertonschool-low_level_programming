b838fe0ffd459e06427ab998bc739b0ba80c063d
parbilla <parbilla@gmail.com>
2021-04-22 15:58:16 +0000

    9

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -14,7 +14,7 @@ int interpolation_search(int *array, size_t size, int value)
 	size_t low = 0;
 	size_t hi = size - 1;
 	size_t pos = low + (((double)(hi - low) / (array[hi] - array[low])) * (value - array[low]));
-	if (array == NULL || size == 0)
+	if (array == NULL || size == 0 || pos >= size)
 		return (-1);
 	if (array[low] > value)
 	{
